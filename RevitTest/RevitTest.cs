1251f5784b1a316f84b7d00c22d1943ed362822a
zhangxinfeng <zhangxinfeng@scene3d.com>
2017-03-14 21:10:39 +0800

    First commit

---
+++ RevitTest/RevitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace RevitTest
+{
+    class RevitTest
+    {
+        public void TestLevel()
+        {
+
+            Level level= new Level();
+            level.get_Parameter(BuiltInParameter.LEVEL_IS_BUILDING_STORY);
+
+            Element element = new Element();
+            element.Name;
+            element.Id;
+            element.LevelId;
+            level.Name;
+            level.Elevation
+        }
+    }
+}
